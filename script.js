711b33ae809fc77f46eb90b4199ca39beaf5f6fb
Shikhar2001Bhadani <shkrbh30052001@gmail.com>
2025-01-27 12:52:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function isSameType(value1, value2) {
   //your js code here
+	let valueone = parseFloat(value1);
+	let valuetwo = parseFloat(value2);
+	if(!isNaN(valueone)&& !isNaN(valuetwo)){
+	return typeof(valueone)===typeof(valuetwo)
+	}
+	return typeof(value1)===typeof(value2)
 }
 
 // do not change the code below.
